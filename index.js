2fd32fa67ceaa25763cd59a25c9eab0659e9c538
Yossef <ym97989390@gmail.com>
2026-01-08 19:42:24 +0200

    feat : bonus

--- index.js
+++ index.js
@@ -171,3 +171,23 @@ function getOSInfo(){
     return {Platform: os.platform(), Arch: os.arch()};
 }
 console.log( getOSInfo());
+
+// bonus 
+
+var findKthPositive = function(arr, k) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        let missing = arr[mid] - (mid + 1);
+
+        if (missing < k) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left + k;
+};
